d011d5d4eeb972b85cd5d3d86bdc7baa26ff6568
Caio <eugeniocaio10@gmail.com>
2020-07-04 17:47:16 -0400

    Util.js added

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ const fetchdata = async (searchTerm) => {
   console.log(response.data);
 };
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
-
 const onInput = (event) => {
   fetchdata(event.target.value);
 };
