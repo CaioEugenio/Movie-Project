767dc587be3286cb344a49c56a888e0f20fd0a43
Caio <eugeniocaio10@gmail.com>
2020-07-04 21:53:23 -0400

    Dropdown now generated from JS

--- index.js
+++ index.js
@@ -25,5 +25,19 @@ const onInput = async (event) => {
   }
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class=input/>
+  <div class="dropdown is-active">
+    <div class="dropdown-menu">
+      <div class="dropdown-menu results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultwrapper = document.querySelector(".results");
+
 input.addEventListener("input", debounce(onInput, 500));
