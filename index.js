2a3cc407c276a1ac88ab8729fb8d17f01fc18b7c
Caio <eugeniocaio10@gmail.com>
2020-07-05 14:34:09 -0400

    Finalized dropdown menu with functionalities

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ const onInput = async (event) => {
 const root = document.querySelector(".autocomplete");
 root.innerHTML = `
   <label><b>Search For a Movie</b></label>
-  <input class=input/>
-  <div class="dropdown is-active">
+  <input class="input"/>
+  <div class="dropdown">
     <div class="dropdown-menu">
-      <div class="dropdown-menu results"></div>
+      <div class="dropdown-content results"></div>
     </div>
   </div>
 `;
@@ -39,5 +39,36 @@ root.innerHTML = `
 const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
 const resultwrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
+  const movies = await fetchdata(event.target.value);
+
+  if (movies.length === 0) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
+  resultwrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
+
+  for (let movie of movies) {
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+        <img src="${imgSrc}"/>
+        ${movie.Title}
+    `;
+
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
+
+    resultwrapper.appendChild(option);
+  }
+};
 
 input.addEventListener("input", debounce(onInput, 500));
