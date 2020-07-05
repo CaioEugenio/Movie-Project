8e3a0108a3fa93ff3335c388413f27821669c452
Caio <eugeniocaio10@gmail.com>
2020-07-04 20:39:20 -0400

    Movies now populate page

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const fetchdata = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
-const onInput = (event) => {
-  fetchdata(event.target.value);
+const onInput = async (event) => {
+  const movies = await fetchdata(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <img src="${movie.Poster}"/>
+        <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 const input = document.querySelector("input");
